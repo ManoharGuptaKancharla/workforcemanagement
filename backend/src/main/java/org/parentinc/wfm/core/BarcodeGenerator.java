76b2b73a2748de7ee52a54b92c0dc361e850bd01
ManoharGuptaKancharla <manoharmanit@gmail.com>
2020-12-04 10:13:14 +0530

    Barcode API without DB integration.

---
+++ backend/src/main/java/org/parentinc/wfm/core/BarcodeGenerator.java
@@ -0,0 +1,22 @@
+package org.parentinc.wfm.core;
+
+import org.krysalis.barcode4j.impl.upcean.EAN13Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class BarcodeGenerator {
+    public BufferedImage generateEAN13BarcodeImage(String barcodeText) {
+        EAN13Bean barcodeGenerator = new EAN13Bean();
+        BitmapCanvasProvider canvas =
+                new BitmapCanvasProvider(160, BufferedImage.TYPE_BYTE_BINARY, false, 0);
+
+        barcodeGenerator.generateBarcode(canvas, barcodeText);
+        return canvas.getBufferedImage();
+    }
+}
